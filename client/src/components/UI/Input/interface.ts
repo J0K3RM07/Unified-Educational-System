474c2656029bc9979b4abe33f80a91a3636203c9
Dmitry <jagotti@yandex.ru>
2023-11-18 11:23:08 +0300

    типизация UI

--- client/src/components/UI/Input/interface.ts
+++ client/src/components/UI/Input/interface.ts
@@ -1,8 +1,8 @@
 export interface IProps {
-  id?: string
-  className?: string
-  label?: string
-  error?: boolean
-  placeholder?: string
-  type?: string
+  id?: string;
+  className?: string;
+  label?: string;
+  error?: boolean;
+  placeholder?: string;
+  type?: string;
 }
