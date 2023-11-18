8c5af32e42f192132c3599466fa652add82de602
Dmitry <jagotti@yandex.ru>
2023-11-18 11:23:08 +0300

    типизация UI

--- client/src/components/UI/Textarea/interface.ts
+++ client/src/components/UI/Textarea/interface.ts
@@ -1,4 +1,5 @@
-export interface IProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+export interface IProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   id?: string;
   className?: string;
   label?: string;
