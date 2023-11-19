42f8022d32b9a09822b07384394bf6bb9d7fd69d
Nikita Zhvalik <130430335+NikitaZhvalik@users.noreply.github.com>
2023-11-19 20:22:19 +1000

    auth change

--- client/src/components/screens/Authorization/Authorization.tsx
+++ client/src/components/screens/Authorization/Authorization.tsx
@@ -1,16 +1,45 @@
-import Footer from "../../layout/footer/Footer";
-import Header from "../../layout/header/Header";
+import { Link } from "react-router-dom";
 
-const Authorization = () => {
-  return (
-    <div className='container'>
-      <Header />
+import {Layout} from "../../layout/Layout/Layout";
+import Button from "../../UI/Button/Button";
+import {Input} from "../../UI/Input/Input";
 
+import styles from "./styles.module.scss"
 
+export const Authorization = () => {
+    return (
+        <div className='container'>
+            <Layout>
+                <div className={styles.auth}>
+                    <form className={styles.form}>
+                        <span className={styles.title}>Авторизация</span>
+                        <Input 
+                            className={styles.input}
+                            label={'ФИО'}
+                            placeholder={'Введите ваше ФИО'}
+                        />
 
-      <Footer />
-    </div>
-  );
-};
+                        <Input 
+                            className={styles.input}
+                            label={'Пароль'}
+                            type={'password'}
+                            placeholder={'Введите пароль'}
+                        />
 
-export default Authorization;
+                        <Button 
+                            className={styles.submit}
+                            children={'Войти'}
+                        />
+
+                    </form>
+                    <div className={styles.regBox}>
+                        <span>Еще нет аккаунта? </span>
+                        <Link to={'/reg'} className={styles.reg}>
+                            Зарегистрироваться
+                        </Link>
+                    </div>
+                </div>
+            </Layout>
+        </div>
+    )
+}
