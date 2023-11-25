e8a5a45523f3c765bdfbd87631d1df1526816eae
Dmitry <jagotti@yandex.ru>
2023-11-25 15:06:44 +0300

    change component, filter work

--- client/src/components/screens/TeacherArea/TeacherArea.tsx
+++ client/src/components/screens/TeacherArea/TeacherArea.tsx
@@ -5,21 +5,33 @@ import { Homework } from "./homework/homework";
 import Heading from "../../UI/Titles/Heading/Heading";
 
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 import styles from "./styles.module.scss";
 
 export const TeacherArea = () => {
+  const [filter, setFilter] = useState({
+    day: "",
+    lesson: "",
+    group: "",
+  });
+
   const params = useParams();
   const pageName = params.name;
 
-  console.log(pageName, params);
+  console.log(filter);
 
   return (
     <Layout>
       <div className={styles.wrapper}>
-        <Heading children={"Рейтинг"} />
-        <Filter />
-        {pageName === "homework" ? <Homework /> : <Table />}
+        {pageName === "homework" ? (
+          <Heading children={"Домашнее задание"} />
+        ) : (
+          <Heading children={"Рейтинг"} />
+        )}
+
+        <Filter change={setFilter} />
+        {pageName === "homework" ? <Homework /> : <Table filter={filter} />}
       </div>
     </Layout>
   );
